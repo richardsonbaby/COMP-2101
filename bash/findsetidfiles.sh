a5e37cb73d1b00b3b4c18a455c6329107af5f6b1
richardsonbaby <richardsonbaby2007@gmail.com>
2019-09-20 08:25:44 -0700

    asd

--- bash/findsetidfiles.sh
+++ bash/findsetidfiles.sh
@@ -18,5 +18,5 @@ find / -type f -executable -perm -2000 -ls 2>/dev/null | sort -k 3
 echo "============================================================================================================"
 echo "Task 2 for the exercise is to modify it to display the 10 largest files in the system, sorted by their sizes"
 echo "============================================================================================================"
-find /home/ -type f -exec ls -alh --block-size=M {} \; | sort -hr -k5 | head -n 10 | awk '{print $5, $3, $9}'
+find / 2>/dev/null -type f -exec ls -lh --block-size=M {} \; | sort -hr -k5 | head 
 echo ""
